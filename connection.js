acf8c969257aef19c8115be8981d03763d60f44c
jasonnonstop10 <jay44411@gmail.com>
2021-07-30 01:46:12 +0700

    [fix] :truck: reconstruction file path

--- connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true});
-
-mongoose.connection.once('open',function(){
-    console.log('connection has been made');
-}).on('error',function(error){
-    console.log('error is:', error)
-})
